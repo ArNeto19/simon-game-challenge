d38c84471d5d249c1d93c9a02f37ce6b05fc58b2
arneto19 <armandopdneto@gmail.com>
2022-10-13 19:33:44 -0300

    General fixes. Corrected score saving handler being called several times

--- game.js
+++ game.js
@@ -1,49 +1,49 @@
-//jshint esversion:6
+//jshint esversion:8
 
 const buttonColours = ['red', 'blue', 'green', 'yellow'];
 let gamePattern = [];
 let userClickedPattern = [];
 let level = 0;
 let playerName = '';
+const apiFetch = fetch('https://evening-temple-42830.herokuapp.com/api/v1/players-scores');
 
-$.get('https://evening-temple-42830.herokuapp.com/api/v1/players-scores', (data) => {
+apiFetch
+  .then(res => res.json())
+  .then(data => {
+    data.sort((x, y) => y.level - x.level);
 
-  data.sort((x, y) => {
-    return y.level - x.level;
-  });
-
-  if (data.length < 5) {
-    data.forEach((player) => {
-      $('#ranking').append(`<li>
-        <mark> ${player.name} </mark>
-        <small> ${player.level} </small>
-        </li>`);
-    });
-  } else {
-    for (let i = 0; i < 5; i++) {
-      $('#ranking').append(`<li>
-        <mark> ${data[i].name} </mark>
-        <small> ${data[i].level} </small>
-        </li>`);
+    if (data.length < 5) {
+      data.forEach((player) => {
+        $('#ranking').append(`<li>
+          <mark> ${player.name} </mark>
+          <small> ${player.level} </small>
+          </li>`);
+      });
+    } else {
+      for (let i = 0; i < 5; i++) {
+        $('#ranking').append(`<li>
+          <mark> ${data[i].name} </mark>
+          <small> ${data[i].level} </small>
+          </li>`);
+      }
     }
-  }
+  })
+  .catch(err => console.error(err));
 
-});
 
-
-$(document).keydown(function(event) {
+$(document).one('keydown', (event) => {
   if (level === 0 && event.key === 'Enter') {
     nextSequence();
   }
 });
 
-$('.btn').click(function() {
+$('.btn').click((event) => {
   if (level === 0) {
-    setTimeout(function() {
+    setTimeout(() => {
       nextSequence();
     }, 500);
-  } else if (level > 0) {
-    userChosenColour = this.id;
+  } else {
+    userChosenColour = event.target.id;
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
@@ -79,69 +79,54 @@ function animatePress(currentColour) {
 
   $(activeButton).addClass('pressed');
 
-  setTimeout(function() {
+  setTimeout(() => {
     $(activeButton).removeClass('pressed');
   }, 100);
 }
 
 function checkAnswer(currentLevel) {
   if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
-    console.log('success');
     if (userClickedPattern.length === gamePattern.length) {
-      setTimeout(function() {
+      setTimeout(() => {
         nextSequence();
       }, 1000);
     }
   } else {
-    console.log('wrong');
     gameOver();
   }
 }
 
 function gameOver() {
   playSound('wrong');
-  $('#level-title').html('');
-  $('#level-title').html('<h1>Game Over!</h1> <p>CLICK HERE or press Enter to save score</p>');
+  $('#level-title').html("<h1>Game Over!</h1> <p id='save-score'>CLICK HERE to save your score</p>");
   $('body').addClass('game-over');
-  setTimeout(function() {
+  setTimeout(() => {
     $('body').removeClass('game-over');
   }, 200);
   startOver();
 }
 
-function startOver() {
+function postScore() {
+  playerName = prompt('Player Name');
 
-  $('#level-title').click(() => {
-    playerName = prompt('Player Name');
-
+  return new Promise((resolve, reject) => {
     if (playerName === null || playerName === '') {
       alert('Your score could not be saved! Please enter a valid name.');
+      reject(new Error('Invalid playername. Nothing to POST'));
+      startOver();
     } else {
       $.post('https://evening-temple-42830.herokuapp.com/api/v1/players-scores', {
         level,
         playerName
       });
-      window.location.reload();
+      resolve();
     }
   });
+}
 
-  $(document).keydown(function(event) {
-
-    if (event.key === 'Enter') {
-      playerName = prompt('Player Name');
-
-      if (playerName === null || playerName === '') {
-        alert('Your score could not be saved! Please enter a valid name.');
-      } else {
-        $.post('https://evening-temple-42830.herokuapp.com/api/v1/players-scores', {
-          level,
-          playerName
-        });
-        window.location.reload();
-      }
-
-    }
-
+function startOver() {
+  $('#save-score').one('click', async (event) => {
+    await postScore();
+    window.location.reload();
   });
-
 }
